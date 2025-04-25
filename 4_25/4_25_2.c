5d8708c098415942a09d82ad027b6044343c0e8d
mi00217 <whb-070-fsp@e-net.nara.jp>
2025-04-25 06:22:37 +0000

    4_25

---
+++ 4_25/4_25_2.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int number;
+  number=8765 ;//ここで変数に10を代入して表示しよう
+   printf("number=%d\n",number); //変数numberの値を出力
+    return 0;
+}
---
+++ 4_25/4_25_3.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(void){
+    int a,b;
+    //入力値の読み取り
+    scanf("%d",&a);
+    scanf("%d",&b);
+    printf("%d+%d=%d",a,b,a+b);
+    return 0;
+}
