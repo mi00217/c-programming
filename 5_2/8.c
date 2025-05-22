6bcae6092daaecc877eae0ea54d17fbd016dacca
mi00217 <whb-070-fsp@e-net.nara.jp>
2025-05-22 02:29:51 +0000

    5_21補習

--- 5_2/8.c
+++ 5_2/8.c
@@ -2,8 +2,10 @@
 int main(void){
     for(int i=1; i<=10;i++){
         for(int j=1;j<=10;j++){
-            printf("%d",i*j);
+            printf("%4d",i*j);
         }
-        return 0;
+        printf("\n");
+
     }
+    return 0;
 }
